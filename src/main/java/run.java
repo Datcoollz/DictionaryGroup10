79356fe95e7858f5a3d6d9be9e1ac4bcce62dd7e
arashi1224 <40011448+arashi1224@users.noreply.github.com>
2021-10-08 23:59:33 +0700

    Update run.java

--- src/main/java/run.java
+++ src/main/java/run.java
@@ -1,14 +1,29 @@
-import control.DictionaryCommandLine;
 import control.DictionaryManagement;
+import control.DictionaryCommandLine;
+import model.Word;
 import model.Dictionary;
+import java.util.Scanner;
 
-/**
- * Main program run.
- */
 public class run {
-    private static final String file_name = "dictionaries.txt";
+
     public static void main(String[] args) {
+        DictionaryCommandLine command = new DictionaryCommandLine();
         Dictionary dictionary = new Dictionary();
-        DictionaryCommandLine.dictionaryBasic(DictionaryCommandLine.SHOW_ALL_WORD, dictionary);
+        command.dictionaryAdvanced("insert from file", dictionary);
+        command.dictionaryAdvanced("show all word", dictionary);
+        System.out.printf("available command:\n"
+                + "-insert from file"
+                + "-insert from commandline"
+                + "-show all word"
+                + "-look up"
+                + "close (To quit using dictionary)");
+        System.out.println("enter command:");
+        String input_order;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            input_order = scanner.nextLine();
+            command.dictionaryBasic(input_order, dictionary);
+
+        }while(!input_order.equals("close"));
     }
 }
