6797f9718d53c69e9d8f646858e394eb432cfb30
arashi1224 <40011448+arashi1224@users.noreply.github.com>
2021-10-09 15:12:04 +0700

    add word chuyển sang đây, look up thành void
    thêm export to file, add, remove, fix word.

--- src/main/java/control/DictionaryManagement.java
+++ src/main/java/control/DictionaryManagement.java
@@ -6,6 +6,7 @@ import model.Word;
 import java.io.File;
 import java.util.Objects;
 import java.util.Scanner;
+import java.io.PrintWriter;
 import java.io.FileNotFoundException;
 
 public class DictionaryManagement {
@@ -20,16 +21,16 @@ public class DictionaryManagement {
         String input_word;
         String input_definition;
         for (int i = 0; i < word_num; i++) {
-            input_word = input.next();
+            input_word = input.nextLine();
             input_definition = input.nextLine();
-            dictionary.addWord(input_word, input_definition);
+            dictionary.getWord_list().add(new Word(input_word, input_definition));
         }
         input.close();
     }
 
     public static void insertFromFile(Dictionary dictionary) {
         try {
-            File file = new File("document.txt");
+            File file = new File("dictionaries.txt");
             Scanner input = new Scanner(file);
             while (input.hasNextLine()) {
                 String data = input.nextLine();
@@ -42,15 +43,78 @@ public class DictionaryManagement {
         }
     }
 
-
-    public static String dictionaryLookup(Dictionary dictionary, String word_target) {
+    /**
+     * tim kiem nghia cua tu.
+     * @return tra ve nghia cua tu
+     */
+    public static void dictionaryLookup(Dictionary dictionary) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("tim tu:");
+        String word_target = input.nextLine();
         for (Word i : dictionary.getWord_list()) {
             if (Objects.equals(i.getWord_target(), word_target)) {
-                return i.getWord_explain();
+                System.out.println("nghia cua"
+                        + word_target + " la: "
+                        + i.getWord_explain());
             }
         }
         System.out.println("Word not found.");
-        return "";
+    }
+
+
+    /**
+     * Add a new word to the list
+     */
+    public static void addWord(Dictionary dictionary) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("them tu:");
+        String word_target = input.nextLine();
+        System.out.print("nghia cua tu");
+        String word_explain = input.nextLine();
+        dictionary.getWord_list().add(new Word(word_target, word_explain));
+    }
+
+    /**
+     * remove a word.
+     */
+    public static void removeWord(Dictionary dictionary) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("xoa tu:");
+        String word_target = input.nextLine();
+        dictionary.getWord_list().removeIf(i -> Objects.equals(i.getWord_target(), word_target));
+    }
+
+    /**
+     * change a word's definition;
+     * @param dictionary
+     */
+    public static void fixWord(Dictionary dictionary) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("sua tu:");
+        String word_target = input.nextLine();
+        System.out.print("nghia moi:");
+        String new_word_explain = input.nextLine();
+        for (Word i : dictionary.getWord_list()) {
+            if (Objects.equals(i.getWord_target(), word_target)) {
+                i.setWord_explain(new_word_explain);
+            }
+        }
+    }
+
+    /**
+     * xuat du lieu ra file.
+     */
+    public static void dictionaryExportTofile(Dictionary dictionary) {
+        File file = new File("dictionaries.txt");
+        try {
+            PrintWriter output = new PrintWriter(file);
+            for (Word i : dictionary.getWord_list()) {
+                output.println(i.getWord_target() + "\t" + i.getWord_explain());
+            }
+            output.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("No file found");
+        }
     }
 
 }
