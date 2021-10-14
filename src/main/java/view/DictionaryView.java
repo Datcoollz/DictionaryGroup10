64b590443d9436c603d210767ed432861d93cf70
Datcoollz <ngoquydat02@gmail.com>
2021-10-15 01:38:04 +0700

    view: add confirm when exit window that writes to file Change to Vietnamese interface Change settings to different buttons Resize window

--- src/main/java/view/DictionaryView.java
+++ src/main/java/view/DictionaryView.java
@@ -1,17 +1,14 @@
 package view;
 
 import control.DictionaryCommandLine;
+import control.DictionaryManagement;
 import model.Dictionary;
 import model.Word;
 
 import javax.swing.*;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 
 /**
  * Display for the app.
@@ -25,7 +22,6 @@ public class DictionaryView {
     private final JList<Word> wordTargetList;
     DefaultListModel<Word> wordListModel = new DefaultListModel<>();
     ListSelectionModel selectionModel;
-    //Position value
 
 
     public DictionaryView(Dictionary dictionary) {
@@ -34,14 +30,26 @@ public class DictionaryView {
         Container actionContainer;
         {
             mainFrame = new JFrame("dictionary");
-            mainFrame.setBounds(450, 90, 450, 600);
+            mainFrame.setBounds(400, 70, 600, 640);
             mainFrame.setLayout(null);
-            mainFrame.setDefaultCloseOperation(mainFrame.EXIT_ON_CLOSE);
+            //mainFrame.setDefaultCloseOperation(mainFrame.EXIT_ON_CLOSE);
+            mainFrame.addWindowListener(new java.awt.event.WindowAdapter() {
+                @Override
+                public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+                    if (JOptionPane.showConfirmDialog(mainFrame,
+                            "Bạn có chắc chắn muốn thoát?", "Thoát?",
+                            JOptionPane.YES_NO_OPTION,
+                            JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION) {
+                        DictionaryManagement.dictionaryExportToFile(dictionary);
+                        System.exit(0);
+                    }
+                }
+            });
 
             actionContainer = mainFrame.getContentPane();
             actionContainer.setLayout(null);
-        }
 
+        }
 
         //search field & button.
         {
@@ -51,85 +59,77 @@ public class DictionaryView {
             wordSearchField.setLocation(0, 2);
             actionContainer.add(wordSearchField);
             //Search button
-            JButton wordSearchButton = new JButton("search");
-            wordSearchButton.setSize(100, 25);
+            JButton wordSearchButton = new JButton("Tìm");
+            wordSearchButton.setSize(75, 25);
             wordSearchButton.setLocation(225, 2);
-            wordSearchButton.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    String wordKey = wordSearchField.getText();
-                    Dictionary searchDictionary = DictionaryCommandLine.dictionaryAdvanced(
-                            DictionaryCommandLine.SEARCH_KEY_WORD,
-                            dictionary,
-                            new Word(wordKey,""));
-                    assert searchDictionary != null;
-                    updateWordList(searchDictionary);
-                }
+            wordSearchButton.addActionListener(e -> {
+                String wordKey = wordSearchField.getText();
+                Dictionary searchDictionary = DictionaryCommandLine.dictionaryAdvanced(
+                        DictionaryCommandLine.SEARCH_KEY_WORD,
+                        dictionary,
+                        new Word(wordKey, ""));
+                assert searchDictionary != null;
+                updateWordList(searchDictionary);
             });
             actionContainer.add(wordSearchButton);
         }
 
-
-        //settings: add word, remove word, fix word.
+        //Buttons
         {
-            JComboBox<String> settings = new JComboBox<>(new String[]{"settings", "add word", "fix word", "remove word"});
-            settings.setBounds(330, 2, 95, 25);
-            settings.setSelectedItem(settings);
-            settings.addItemListener(new ItemListener() {
+            JButton addWord = new JButton("Thêm");
+            addWord.setBounds(340, 2, 80, 25);
+            addWord.addActionListener(new ActionListener() {
                 @Override
-                public void itemStateChanged(ItemEvent e) {
-                    if (e.getStateChange() == ItemEvent.SELECTED) {
-                        String item = e.getItem().toString();
-                        String wordTarget;
-                        String word_explain;
-                        switch (item) {
-                            case "add word":
-                                wordTarget = JOptionPane.showInputDialog(null, "từ thêm:");
-                                word_explain = JOptionPane.showInputDialog(null, "Y nghĩa");
-                                DictionaryCommandLine.dictionaryAdvanced(
-                                        DictionaryCommandLine.ADD_WORD,
-                                        dictionary,
-                                        new Word(wordTarget, word_explain));
-                                updateWordList(dictionary);
-                                break;
-                            case "remove word":
-                                wordTarget = JOptionPane.showInputDialog(null, "chon từ xóa:");
-                                DictionaryCommandLine.dictionaryAdvanced(
-                                        DictionaryCommandLine.REMOVE_WORD,
-                                        dictionary, new Word(wordTarget, ""));
-                                updateWordList(dictionary);
-                                break;
-                            case "fix word":
-                                wordTarget = JOptionPane.showInputDialog(null, "chon từ sửa:");
-                                word_explain = JOptionPane.showInputDialog(null, "Y nghĩa mới:");
-                                DictionaryCommandLine.dictionaryAdvanced(
-                                        "fix word",
-                                        dictionary,
-                                        new Word(wordTarget, word_explain));
-                                updateWordList(dictionary);
-                                break;
-                            case "settings":
-                                DictionaryCommandLine.dictionaryAdvanced("show all word", dictionary);
-                                break;
-                            default:
-                                break;
-                        }
-                    }
-
+                public void actionPerformed(ActionEvent e) {
+                    String wordTarget = JOptionPane.showInputDialog(null, "Từ để thêm:");
+                    String word_explain = JOptionPane.showInputDialog(null, "Giải thích:");
+                    DictionaryCommandLine.dictionaryAdvanced(
+                            DictionaryCommandLine.ADD_WORD,
+                            dictionary,
+                            new Word(wordTarget, word_explain));
+                    updateWordList(dictionary);
                 }
             });
-            actionContainer.add(settings);
+            JButton removeWord = new JButton("Xóa");
+            removeWord.setBounds(420, 2, 80, 25);
+            removeWord.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    String wordTarget = JOptionPane.showInputDialog(null, "Từ cần xóa:");
+                    DictionaryCommandLine.dictionaryAdvanced(
+                            DictionaryCommandLine.REMOVE_WORD,
+                            dictionary, new Word(wordTarget, ""));
+                    updateWordList(dictionary);
+                }
+            });
+            JButton fixWord = new JButton("Sửa");
+            fixWord.setBounds(500, 2, 80, 25);
+            fixWord.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    String wordTarget = JOptionPane.showInputDialog(null, "Từ cần sửa:");
+                    String word_explain = JOptionPane.showInputDialog(null, "Giải thích mới:");
+                    DictionaryCommandLine.dictionaryAdvanced(
+                            "fix word",
+                            dictionary,
+                            new Word(wordTarget, word_explain));
+                    updateWordList(dictionary);
+                }
+            });
+            actionContainer.add(addWord);
+            actionContainer.add(removeWord);
+            actionContainer.add(fixWord);
         }
 
         //labels
         {
             JLabel wordTargetListLabel = new JLabel("Từ vựng");
-            wordTargetListLabel.setBounds(0, 30, 225, 30);
+            wordTargetListLabel.setBounds(0, 30, 300, 30);
             wordTargetListLabel.setFont(new Font("Arial", Font.BOLD, 25));
             actionContainer.add(wordTargetListLabel);
 
-            JLabel wordExplainLabel = new JLabel("Y nghĩa");
-            wordExplainLabel.setBounds(225, 30, 225, 30);
+            JLabel wordExplainLabel = new JLabel("Ý nghĩa");
+            wordExplainLabel.setBounds(300, 30, 300, 30);
             wordExplainLabel.setFont(new Font("Arial", Font.BOLD, 25));
             actionContainer.add(wordExplainLabel);
         }
@@ -137,30 +137,27 @@ public class DictionaryView {
         //wordTarget list
         {
             wordTargetList = new JList<>();
-            wordTargetList.setBounds(0, 60, 220, 540);
+            wordTargetList.setBounds(0, 60, 299, 540);
             wordTargetList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
             wordTargetList.setLayoutOrientation(JList.VERTICAL);
             wordTargetList.setVisibleRowCount(-1);
             updateWordList(dictionary);
             selectionModel = this.wordTargetList.getSelectionModel();
-            selectionModel.addListSelectionListener(new ListSelectionListener() {
-                @Override
-                public void valueChanged(ListSelectionEvent e) {
-                    if (!e.getValueIsAdjusting()) {
-                        updateWordExplain();
-                    }
+            selectionModel.addListSelectionListener(e -> {
+                if (!e.getValueIsAdjusting()) {
+                    updateWordExplain();
                 }
             });
             JScrollPane scrollPane = new JScrollPane();
             scrollPane.setViewportView(wordTargetList);
-            scrollPane.setBounds(0, 60, 220, 540);
+            scrollPane.setBounds(0, 60, 299, 540);
             actionContainer.add(scrollPane);
         }
 
         //word explain area
         {
             wordExplainArea = new JTextArea();
-            wordExplainArea.setBounds(230, 60, 220, 540);
+            wordExplainArea.setBounds(301, 60, 299, 540);
             actionContainer.add(wordExplainArea);
         }
 
@@ -191,4 +188,3 @@ public class DictionaryView {
         }
     }
 }
-
