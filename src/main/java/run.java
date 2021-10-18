c0e3884319fb58243a2c08818ea228a314142dc0
Datcoollz <ngoquydat02@gmail.com>
2021-10-19 00:31:39 +0700

    Update run file for testing

--- src/main/java/run.java
+++ src/main/java/run.java
@@ -5,13 +5,27 @@ import view.DictionaryView;
 public class run {
     public static void main(String[] args) {
         Dictionary dictionary = new Dictionary();
-        DictionaryCommandLine.dictionaryAdvanced(DictionaryCommandLine.INSERT_FROM_FILE, dictionary);
-        DictionaryCommandLine.dictionaryAdvanced(DictionaryCommandLine.SHOW_ALL_WORD, dictionary);
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                DictionaryView app = new DictionaryView(dictionary);
-                app.updateWordList(dictionary);
-            }
-        });
+        dictionary.insert("abE");
+        dictionary.insert("aDE");
+        dictionary.insert("DCwweaXE");
+        dictionary.insert("CASEX");
+        dictionary.insert("bsAAx");
+        dictionary.insert("bRScc");
+        dictionary.insert("a");
+        dictionary.insert("ABG");
+
+        System.out.println(dictionary.searchPrefix("a"));
+        System.out.println(dictionary.searchPrefix("ab"));
+        System.out.println(dictionary.searchPrefix("b"));
+        System.out.println(dictionary.searchAll());
+
+//        DictionaryCommandLine.dictionaryAdvanced(DictionaryCommandLine.INSERT_FROM_FILE, dictionary);
+//        DictionaryCommandLine.dictionaryAdvanced(DictionaryCommandLine.SHOW_ALL_WORD, dictionary);
+//        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+//            public void run() {
+//                DictionaryView app = new DictionaryView(dictionary);
+//                app.updateWordList(dictionary);
+//            }
+//        });
     }
 }
