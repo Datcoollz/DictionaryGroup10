861e36a66216565ad3a8c169c2a0ddfd6edfb646
Datcoollz <ngoquydat02@gmail.com>
2021-10-21 00:36:06 +0700

    Add closing panel, fix functions not updating list properly, revamp and minor optimization.

--- src/main/java/view/DictionaryView.java
+++ src/main/java/view/DictionaryView.java
@@ -1,19 +1,18 @@
 package view;
 
-import control.*;
+import control.DictionaryCommandLine;
+import control.TextToSpeech;
+import control.Translator;
 import model.Dictionary;
 import model.Word;
 
 import javax.swing.*;
-import javax.swing.border.AbstractBorder;
+import javax.swing.border.LineBorder;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class DictionaryView {
     private JPanel mainPanel;
@@ -21,7 +20,6 @@ public class DictionaryView {
     private JPanel googleAPI;
     private JSplitPane mainDictionary;
     private JTextField wordSearchField;
-    private JButton wordSearchButton;
     private JButton addButton;
     private JEditorPane wordExplainArea;
     private JScrollPane searchScrollPane;
@@ -44,6 +42,7 @@ public class DictionaryView {
     private JScrollPane targetScrollPane;
     private JButton swap;
     private JLabel home;
+    private JLabel searchSign;
     DefaultListModel<Word> wordListModel = new DefaultListModel<>();
     ListSelectionModel selectionModel;
     private final CardLayout card = (CardLayout) main.getLayout();
@@ -54,6 +53,18 @@ public class DictionaryView {
         frame.setBounds(200, 30, 700, 400);
         frame.pack();
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.addWindowListener(new java.awt.event.WindowAdapter() {
+            @Override
+            public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+                if (JOptionPane.showConfirmDialog(frame,
+                        "Bạn có chắc chắn muốn thoát?", "Thoát?",
+                        JOptionPane.YES_NO_OPTION,
+                        JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION) {
+                    DictionaryCommandLine.dictionaryAdvanced(DictionaryCommandLine.EXPORT_TO_FILE, dictionary);
+                    System.exit(0);
+                }
+            }
+        });
 
         //menu
         home.setIcon(new ImageIcon("src\\main\\java\\image\\home.png"));
@@ -75,31 +86,30 @@ public class DictionaryView {
         API.setRolloverIcon(hover);
         API.addActionListener(e -> card.show(main, "googleAPI"));
 
-        //search field & button.
-        {
-            //Search button-> get from wordSearchField
-            wordSearchButton.setFocusPainted(false);
-            wordSearchButton.setContentAreaFilled(false);
-            wordSearchButton.setIcon(new ImageIcon("src\\main\\java\\image\\search.png"));
-            wordSearchButton.addActionListener(e -> {
-                String wordKey = wordSearchField.getText();
-                if(!wordKey.equals("")) {
-                    Dictionary searchDictionary = DictionaryCommandLine.dictionaryAdvanced(
-                            DictionaryCommandLine.SEARCH_KEY_WORD, dictionary,
-                            new Word(wordKey, ""));
-                    assert searchDictionary != null;
-                    updateWordList(searchDictionary);
-                }
-                wordExplainArea.setText("this help to clear a single dot");
-                wordExplainArea.setText("");
-            });
-        }
+        //search field , 3 override như nhau.
+        searchSign.setIcon(new ImageIcon("src\\main\\java\\image\\search.png"));
+        wordSearchField.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                updateSearch(dictionary);
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                updateSearch(dictionary);
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                updateSearch(dictionary);
+            }
+        });
 
         //wordTarget list
         {
             wordTargetList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
             wordTargetList.setVisibleRowCount(-1);
-            updateWordList(dictionary);
+            updateWordList(dictionary.searchAll());
             selectionModel = this.wordTargetList.getSelectionModel();
             selectionModel.addListSelectionListener(e -> {
                 if (!e.getValueIsAdjusting()) {
@@ -110,36 +120,114 @@ public class DictionaryView {
 
         //popupMenu for fix & remove
         JPopupMenu popupMenu = new JPopupMenu();
-        popupMenu.add(new JMenuItem("fix")).addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                int index = wordTargetList.getSelectedIndex();
-                DictionaryCommandLine.dictionaryAdvanced("fix word",
-                        dictionary, new Word(wordListModel.getElementAt(index).getWordTarget(),
-                                wordExplainArea.getText()));
-                updateWordList(dictionary);
-
-            }
+        popupMenu.add(new JMenuItem("fix")).addActionListener(e -> {
+            int index = wordTargetList.getSelectedIndex();
+            DictionaryCommandLine.dictionaryAdvanced(DictionaryCommandLine.FIX_WORD,
+                    dictionary, new Word(wordListModel.getElementAt(index).getWordTarget(),
+                            wordExplainArea.getText()));
+            updateSearch(dictionary);
         });
-        popupMenu.add(new JMenuItem("remove")).addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                int index = wordTargetList.getSelectedIndex();
-                DictionaryCommandLine.dictionaryAdvanced("remove word",
-                        dictionary, wordListModel.getElementAt(index));
-                updateWordList(dictionary);
-            }
+        popupMenu.add(new JMenuItem("remove")).addActionListener(e -> {
+            int index = wordTargetList.getSelectedIndex();
+            DictionaryCommandLine.dictionaryAdvanced(DictionaryCommandLine.REMOVE_WORD,
+                    dictionary, wordListModel.getElementAt(index));
+            updateSearch(dictionary);
         });
         wordTargetList.setComponentPopupMenu(popupMenu);
 
 
-        //add buttons
-        addButton.setFocusPainted(false);
-        addButton.setBorderPainted(false);
-        addButton.setIcon(new ImageIcon("src\\main\\java\\image\\add.png"));
-        addButton.addActionListener(e -> {
-            //implement later
-        });
+        //add buttons (phần dài nhất)
+        {
+            addButton.setFocusPainted(false);
+            addButton.setBorderPainted(false);
+            addButton.setIcon(new ImageIcon("src\\main\\java\\image\\add.png"));
+            addButton.addActionListener(e -> {
+                String wordInput = JOptionPane.showInputDialog("chọn từ để thêm:");
+                System.out.println(dictionary.search(wordInput));
+                if (dictionary.search(wordInput) != null) {
+                    JOptionPane.showMessageDialog(null, "Từ đã có sẵn");
+                } else if (wordInput.equals("")) {
+                    JOptionPane.showMessageDialog(null, "Từ chưa được nhập");
+                } else {
+                    String wordTarget = wordInput.trim().replaceAll(" +", " ");
+
+                    final String[] last = {"<i>" + wordTarget};
+                    JDialog dialog = new JDialog(frame, "add box");
+                    dialog.setLayout(null);
+                    dialog.setBounds(300, 130, 440, 440);
+
+                    JLabel jLabel1 = new JLabel("phát âm:");
+                    jLabel1.setBounds(11, 11, 50, 18);
+                    JTextField jTextField1 = new JTextField();
+                    jTextField1.setBounds(61, 10, 150, 20);
+                    dialog.add(jLabel1);
+                    dialog.add(jTextField1);
+
+                    JLabel jLabel2 = new JLabel("dạng từ:");
+                    jLabel2.setBounds(215, 11, 50, 18);
+                    JTextField jTextField2 = new JTextField();
+                    jTextField2.setBounds(265, 10, 150, 20);
+                    dialog.add(jLabel2);
+                    dialog.add(jTextField2);
+
+                    JLabel jLabel3 = new JLabel("Ý nghĩa:");
+                    jLabel3.setBounds(11, 31, 50, 18);
+                    JTextArea jTextArea3 = new JTextArea();
+                    jTextArea3.setBounds(11, 49, 200, 300);
+                    jTextArea3.setBorder(new LineBorder(Color.black, 1));
+                    dialog.add(jLabel3);
+                    dialog.add(jTextArea3);
+
+                    JLabel jLabel4 = new JLabel("Ví dụ:");
+                    jLabel4.setBounds(215, 31, 50, 18);
+                    JTextArea jTextArea4 = new JTextArea();
+                    jTextArea4.setBounds(215, 49, 200, 300);
+                    jTextArea4.setBorder(new LineBorder(Color.black, 1));
+                    dialog.add(jLabel4);
+                    dialog.add(jTextArea4);
+
+                    JButton add = new JButton("add");
+                    add.setBounds(185, 350, 70, 30);
+                    add.addActionListener(e1 -> {
+                        if (!jTextField1.getText().equals("")) {
+                            last[0] += " /" + jTextField1.getText() + "/";
+                        }
+                        last[0] += "</i><br/>";
+                        last[0] += jTextField1.getText() + "</i><br/>";
+                        if (!jTextField2.getText().equals("")) {
+                            last[0] += "<ul><li><b><i>" + jTextField2.getText() + "</i></b>";
+                        }
+                        if (!jTextArea3.getText().equals("")) {
+                            for (String line : jTextArea3.getText().split("\\n")) {
+                                last[0] += "<ul><li><font color='#cc0000'><b>" + line + "</b></font></ul></li>";
+                            }
+                        }
+                        if (!jTextArea4.getText().equals("")) {
+                            for (String line : jTextArea4.getText().split("\\n")) {
+                                last[0] += "<ul><li>" + line + "</ul></li>";
+                            }
+                        }
+                        if (!jTextField2.getText().equals("")) { // lặp lại vì đuôi của tag html
+                            last[0] += "</ul></li>";
+                        }
+                        DictionaryCommandLine.dictionaryAdvanced("add word",
+                                dictionary, new Word(wordTarget, last[0]));
+                        dialog.dispose();
+
+                        //Reset search field
+                        if (wordSearchField.getText().isEmpty()) {
+                            //If field is already empty, just update the list
+                            updateWordList(dictionary.searchAll());
+                        } else {
+                            //Else reset the text and update automatically
+                            wordSearchField.setText("");
+                        }
+                    });
+                    dialog.add(add);
+                    dialog.setVisible(true);
+                }
+            });
+        }
 
         //voice
         voice.setBorderPainted(false);
@@ -149,8 +237,7 @@ public class DictionaryView {
             int index = wordTargetList.getSelectedIndex();
             try {
                 new TextToSpeech(wordListModel.getElementAt(index).getWordTarget());
-            }
-            catch (Exception exception) {
+            } catch (Exception exception) {
                 JOptionPane.showMessageDialog(null, "chưa chọn từ");
             }
         });
@@ -162,9 +249,9 @@ public class DictionaryView {
             explainScrollPane.setViewportView(wordExplainArea);
         }
 
-
-
         //google
+        final String[] source = {"vi"};
+        final String[] target = {"en"};
         sourceLang.setFont(new Font("Segoe UI", Font.PLAIN, 14));
         targetLang.setFont(new Font("Segoe UI", Font.PLAIN, 14));
         sourceText.setLineWrap(true);
@@ -173,36 +260,61 @@ public class DictionaryView {
         //buttons
         arrow.setIcon(new ImageIcon("src\\main\\java\\image\\arrowRight.png"));
         arrow.setFocusPainted(false);
-        arrow.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    targetText.setText(Translator.translate("en", "vi", sourceText.getText()));
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
+        arrow.addActionListener(e -> {
+            try {
+                targetText.setText(Translator.translate(source[0], target[0], sourceText.getText()));
+            } catch (IOException ex) {
+                ex.printStackTrace();
             }
         });
         swap.setIcon(new ImageIcon("src\\main\\java\\image\\swap.png"));
-        swap.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
+        swap.addActionListener(e -> {
+            String tmp;
+            tmp = source[0];
+            source[0] = target[0];
+            target[0] = tmp;
+            if (sourceLang.getText().equals("Vietnamese")) {
+                sourceLang.setText("English");
+                targetLang.setText("Vietnamese");
+            } else {
+                sourceLang.setText("Vietnamese");
+                targetLang.setText("English");
             }
         });
+
+        updateWordList(dictionary.searchAll());
         frame.setVisible(true);
     }
 
+    /**
+     * Update word list and explain area after a change in search bar
+     *
+     * @param dictionary input dictionary
+     */
+    private void updateSearch(Dictionary dictionary) {
+        String wordKey = wordSearchField.getText();
+        ArrayList<Word> list = DictionaryCommandLine.dictionaryAdvanced(
+                DictionaryCommandLine.SEARCH_KEY_WORD, dictionary,
+                new Word(wordKey));
+        assert list != null;
+        updateWordList(list);
 
+        wordExplainArea.setText("this help to clear a single dot");
+        wordExplainArea.setText("");
+    }
 
     /**
      * Update wordTargetList when you search for new words.
      *
-     * @param dictionary dictionary to display
+     * @param list list to display
      */
-    public void updateWordList(Dictionary dictionary) {
+    public void updateWordList(ArrayList<Word> list) {
         this.wordListModel.removeAllElements();
-        this.wordListModel.addAll(dictionary.getWordList());
+        if (list != null) {
+            if (!list.isEmpty()) {
+                this.wordListModel.addAll(list);
+            }
+        }
         this.wordTargetList.setModel(this.wordListModel);
     }
 
@@ -212,7 +324,6 @@ public class DictionaryView {
     private void updateWordExplain() {
         int index = this.wordTargetList.getSelectedIndex();
         if (index >= 0) {
-            //String newWord = this.wordListModel.getElementAt(index).getWordTarget();
             String newExplain = "<html>" + this.wordListModel.getElementAt(index).getWordExplain() + "</html";
             wordExplainArea.setText(newExplain);
         }
