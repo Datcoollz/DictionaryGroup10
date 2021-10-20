70262ef106543e94e62b9b190f6101335865caaf
Datcoollz <ngoquydat02@gmail.com>
2021-10-21 00:37:36 +0700

    Clean up run file.

--- src/main/java/run.java
+++ src/main/java/run.java
@@ -1,31 +1,19 @@
 import control.DictionaryCommandLine;
 import model.Dictionary;
+import model.Word;
 import view.DictionaryView;
+//import view.DictionaryView;
 
 public class run {
     public static void main(String[] args) {
         Dictionary dictionary = new Dictionary();
-        dictionary.insert("abE");
-        dictionary.insert("aDE");
-        dictionary.insert("DCwweaXE");
-        dictionary.insert("CASEX");
-        dictionary.insert("bsAAx");
-        dictionary.insert("bRScc");
-        dictionary.insert("a");
-        dictionary.insert("ABG");
 
-        System.out.println(dictionary.searchPrefix("a"));
-        System.out.println(dictionary.searchPrefix("ab"));
-        System.out.println(dictionary.searchPrefix("b"));
-        System.out.println(dictionary.searchAll());
-
-//        DictionaryCommandLine.dictionaryAdvanced(DictionaryCommandLine.INSERT_FROM_FILE, dictionary);
-//        DictionaryCommandLine.dictionaryAdvanced(DictionaryCommandLine.SHOW_ALL_WORD, dictionary);
-//        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-//            public void run() {
-//                DictionaryView app = new DictionaryView(dictionary);
-//                app.updateWordList(dictionary);
-//            }
-//        });
+        DictionaryCommandLine.dictionaryAdvanced(DictionaryCommandLine.INSERT_FROM_FILE, dictionary);
+        DictionaryCommandLine.dictionaryAdvanced(DictionaryCommandLine.SHOW_ALL_WORD, dictionary);
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                DictionaryView app = new DictionaryView(dictionary);
+            }
+        });
     }
 }
